30fdb76a25a4629e7ef31f8ceedd4f2c1e13e72a
LyNguyen <william.lynguyen@gmail.com>
2021-05-22 11:26:17 +0700

    try commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+var expressLayouts = require('express-ejs-layouts');
+
+const app = express();
+
+
+//su dung layout
+app.use(expressLayouts);
+//ban dau cho layout vao main
+app.use('layout','./layouts/main');
+
+//su dung ejs
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+//su dung body parser de lay du lieu tu body
+app.use(bodyParser.urlencoded());
+
+app.get('/',function(req,res){
+    res.render('index',{layout:"./layouts/home"});
+})
