ca56def6b8c69a1f9190f1e2cc2ab8fd5c105390
LyNguyen <william.lynguyen@gmail.com>
2021-05-23 22:08:19 +0700

    create a register page

---
+++ middlewares/user.js
@@ -0,0 +1,26 @@
+const User = require('../models/user.js');
+const asyncHandler = require('express-async-handler');
+module.exports = asyncHandler(async function(req,res,next){
+    //tim userId tren session
+    const {userId} = req.session;
+    //bien de luu thong tin user
+    res.locals.currentUser = null;
+    //kiem tra co user tren session khong
+    if(userId){
+        //neu co thi tim thong tin cua user trong db coi co dung khong
+        const user = await User.findById(userId);
+        //neu tim duoc thong tin user
+        if(user){
+            
+            //luu vao req de su dung ben router
+            req.currentUser = user;
+            //console.log(user);
+            //luu vao de su dung ben view
+            res.locals.currentUser = user;
+        }
+        next();
+    }else{
+        next();
+  
+    }   
+});
